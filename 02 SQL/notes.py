84abf3a7a40585def172ce0159f71a1c24603d0c
Ibrahim <ibrahim.fakhry93@yahoo.com>
2025-11-01 17:07:39 +0200

    hello

--- 02 SQL/notes.py
+++ 02 SQL/notes.py
@@ -1,7 +1,11 @@
 #
 # * Kiwi@VM4Academy
 
+# ^ look up the slides very important
 
 #! 27 SQL Data Types:
 
 # * BIGINT: used for Citizen ID
+
+
+# * char(n): fixed-length (pre-known number of characters for the string)
--- 02 SQL/notes.sql
+++ 02 SQL/notes.sql
@@ -1,3 +1,5 @@
+
+-- ^ look up the slides very important
 CREATE TABLE hr.departments (
     departmentID int,
     name varchar(20),
@@ -20,6 +22,12 @@ VALUES (3, 'Marketing', 2);
 --* delete from hr.departments where departmentid = 3;
 
 SELECT * FROM hr.departments;
+
+--! Delete table itself (Delete table structure)
+drop table departments ;
+
+--* show which schema we are on now:
+SHOW search_path;
 
 
 --^ Arithmetic
@@ -46,17 +54,17 @@ insert into hr.departments (departmentid,name,managerid) values (6,'other',15);
 
 SELECT * FROM hr.departments;
 
+
+
+--^ Distinct with columns: remove duplicates
+
+select distinct name from departments
 SELECT name FROM hr.departments;
 
---! Delete table itself
-
---* drop table hr.departments ;
-
-
---^ Distinct: remove duplicates
-
-select distinct * from hr.departments
-
+--^ Distinct with select * (all rows)
+select distinct * from departments;
+--! it won't remove the duplicates, because * will work on row level where there are different values and no duplication
+SELECT * FROM hr.departments;
 --!  distinct here won't remove the duplicates, because it works on full duplicates rows not only single field
 
 
@@ -66,6 +74,7 @@ insert into hr.departments(departmentid,name,managerid) values(10," ",1);
 insert into hr.departments(departmentid,name,managerid) values(11,"",2);
 insert into hr.departments(departmentid,name,managerid) values(12,NULL,NULL);
 
+--^ null doesn't work with (=) operator
 --^ null and (is) clause
 select * from hr.departments where name is null;
 
@@ -75,15 +84,18 @@ select * from hr.departments where name is null or managerid is null;
 --^ Exclude NULL
 select * from hr.departments where name is not null;
 
---^ Replace NULL
+--^ Replace NULL: use coalesce(col name,"replaced value")
 
-select departmentid,coalesce(name,"NO Dept") as name_not_null, managerid from hr.departments
+select departmentid,coalesce(name,"NO Dept") as name_not_null, managerid from hr.departments;
+
+--^ replace null but for numeric value as manager_id
+select departmentid,coalesce(name,"NO Dept") as name_not_null, coalesce(manager_id,0) from hr.departments
 
 --^ note:
 --* instead of coalesce in postgres, in my sql there is: nvl(name,"NO dept")
 
 --*=====================================================================================
-
+-- ^ look up the slides very important
 --! filtering: 30. Employees Where conditions
 
 select employeeid,firstname,lastname,birthdate,notes from employees
@@ -96,18 +108,24 @@ select employeeid,firstname,lastname from employees where employeeid < 5
 
 select employeeid,firstname,lastname from employees where employeeid <= 5
 
---~ between include 3 and 5
+--~ between include 3 and 5: continuos range
+--^ Used when you want to filter rows within a continuous range of values.
 select employeeid,firstname,lastname from employees where employeeid between 3 and 5
 
---~ in certain range
+--~ in: set of numbers
+--^ Used when you want to filter rows that match specific, discrete values (not necessarily continuous).
 select employeeid,firstname,lastname from employees where employeeid in (1,3,7,9)
 
 
 --*=====================================================================================
 
 --! 31. Conditions and Sorting
+-- ^ look up the slides very important
 
---^ wildcard:
+--^ wildcard: like + "_" or "%"
+
+--* '_': single character
+--* '%': multi characters or even no characters
 
 select employeeid,firstname,lastname from employees where firstname like'n%';
 
@@ -130,10 +148,13 @@ select employeeid,firstname,lastname from employees order by firstname asc;
 select employeeid,firstname,lastname from employees order by firstname desc;
 
 --*=====================================================================================
+--! 32 Joins
 
+-- ^ look up the slides very important
+--*=====================================================================================
 --! 33. Hands -on SQL Joins
---* Alter - add department id to employees table
 
+--* Alter - add department id to employees table
 ALTER TABLE employees ADD COLUMN Departmentid INT;
 
 --* Update
@@ -160,7 +181,7 @@ ORDER BY employeeid ASC;
 
 --! Exercise
 
---^ Req: list all employess that their departments already exist in the company
+--^ Req: list all employees that their departments already exist in the company
 
 --~ first method: (join): more efficient regarding performance
 
@@ -178,23 +199,24 @@ INNER JOIN departments d
 --*============================================================================
 
 --! 34. SubQuery
-
+-- ^ look up the slides very important
 select * from departments d ;
 select * from employees e ;
+
+--^ Req: list all employees that their departments already exist in the company
+
+--~ Second method, subquery: more complex and less efficient
 
 --* start by writing inner query
 --* outer query will execute after inner query
 
---* use (in) when inner query return mulit-values
+--* use (in) when inner query return multi-values
 select * from employees where departmentid in (select department_id from departments)
 
 --* use (=) when inner query return single value
 select * from employees where departmentid = (select department_id from departments = 1)
 
-
---^ Req: list all employess that their departments already exist in the company
-
---~ Second method, subquery: more complex ad less efficient
+--~ same requirement but we need to display department name
 
 SELECT e.firstname || ' ' || e.lastname AS fullname,
        (SELECT d."name"
@@ -206,6 +228,10 @@ WHERE e.department_id IN (SELECT departmentid FROM departments);
 --*============================================================================
 
 --! 35. Union and Union ALL
+-- ^ look up the slides very important
+--TODO : List all possible names in the table whether are first name or last name
+
+--! interview:
 
 --^ Union: remove duplicates, so it's slower
 --~ example: if first name and lastname have same values
@@ -221,8 +247,8 @@ select lastname as Name from employees;
 --*=====================================================================================
 
 --! 36 Aggregation
-
---* Used alot in report writing, data analysis and data engineering
+-- ^ look up the slides very important
+--* Used a lot in report writing, data analysis and data engineering
 --* تجميع داتا
 
 select * from orders;
@@ -246,23 +272,50 @@ SELECT MIN(quantity), MAX(quantity) FROM order_details;
 SELECT * FROM order_details;
 
 --^ Build a report to show the number of items sold (quantity) for each order.
-select orderid , sum(quantity) as items_sold_per_order from order_detai
+select orderid , sum(quantity) as items_sold_per_order from order_details;
 
 --*=====================================================================================
 
 --! 37 Window Functions
 
---* let's say we have 3 departments in a company, and every department there are 10 employees with 10 salaries
---* req: sort salaries for each department apart
+--* window: to use subset of data 
+
+-- ^ look up the slides very important
+--! interview: types of window functions, most common in interview: Rank()/Dense_Rank
+
+--* use cases: sort student rank for each school classes apart (ranking)
+
 
 --? Example:
 select * from products p;
 --* products table: productname, supplier_id,categoryid,unit,price
 
---^ req: sort the products so I have an order of prices (desc) per each category
+--^ req: sort the products according to their prices (desc) per each category apart
 --* هاتلى ترتيب المنتجات من اعلى سعر لاقل سعر لكل كاتورجي
 
 select productname,categoryid,price ,rank() over(partition by categoryid order by price desc) as price_rank from products;  
+
+
+--TODO : ranking
+--* let's say we have 3 departments in a company, and every department there are 10 employees with 10 salaries
+--! req: sort employees according to their salaries for each department apart
+
+select e.firstname as emp_name, e.department_id, e.salary , rank() over(partition by e.department_id order by e.salary desc) as salary_rank from employees e
+
+
+--^ to display department name, we use join
+
+select e.firstname as emp_name, d."name" as depart_name, e.salary , 
+rank() over(partition by e.department_id order by e.salary desc) as salary_rank from employees e inner join departments d on e.department_id =d.departmentid 
+
+
+
+--^ lead or lag: (ask chatgpt)
+
+--* to get for example the immediate student precedes a certain student
+--* or to get the employee whose salary is immediate higher than a certain employee 
+
+--^ Rank (ask chatgpt): skip the ranks when data is duplicated (ex. different products have same price)
 
 --*=====================================================================================
 --! 38 CTE
